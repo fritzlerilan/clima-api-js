f298ba7be9f26bfaa55eb1d3fe89e5ac3f1b3a48
fritzlerilan <fritzlerilan@gmail.com>
2021-10-04 10:35:34 -0300

    show content response

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const buscarInput = document.querySelector('#buscar');
 const form = document.querySelector('#formulario');
 const container = document.querySelector('#principal');
+const spinner = document.querySelector('#spinner');
+const cityInfo = document.querySelector('#city-info');
+const weatherIcon = document.querySelector('#icon img');
+const tarjetaClima = document.querySelector('#tarjeta-clima');
 
 form.addEventListener('submit', getWeather);
 
@@ -17,11 +21,13 @@ function getWeather(e) {
 }
 
 function consultarAPI(location) {
+    limpiarHTML();
     const apiKey = 'd1e759b3c1d139fe882293457dbb5bc5';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
-
+    spinner.classList.remove('hidden');
     fetch(url)
         .then(res => {
+            spinner.classList.add('hidden');
             if (res.status === 200) {
                 return res.json();
             } else {
@@ -32,7 +38,56 @@ function consultarAPI(location) {
 }
 
 function imprimirClima(clima){
-    console.log(clima);
+    const { main:{ temp, temp_min, temp_max, pressure }, weather} = clima;
+    const centigrados = kelvinToCelsius(temp);
+    
+    weatherIcon.src = `http://openweathermap.org/img/wn/${weather[0].icon}.png`;
+    weatherIcon.alt = weather[0].description;
+    // fill the image 'weatherIcon' to the parentElement and align it vertically and horizontally
+    weatherIcon.style.cssText = 'width: 100%; object-position: center;';
+
+    const divTemperatura = document.createElement('div');
+
+    divTemperatura.style.justifyContent = 'center';
+    
+    const actual = document.createElement('p');
+    actual.style.display = 'inline-block';
+    actual.innerHTML = `${centigrados} &#8451;`;
+    actual.classList.add('fs-1', 'fw-bold');
+
+    divTemperatura.appendChild(actual);
+
+    const titulo = document.createElement('p');
+    titulo.textContent = `Clima en ${clima.name}`;
+
+    const resultadoDiv = document.createElement('div');
+    resultadoDiv.classList.add('text-center');
+
+    const minmax = document.createElement('p');
+    minmax.innerHTML = `<span class="fw-bold">Min:</span> ${kelvinToCelsius(temp_min)} &#8451; <span class="fw-bold">Max:</span> ${kelvinToCelsius(temp_max)} &#8451;`;
+
+    const presion = document.createElement('p');
+    presion.innerHTML = `<span class="fw-bold">Presion:</span> ${pressure} hPa`;
+
+
+    resultadoDiv.appendChild(titulo);
+    resultadoDiv.appendChild(divTemperatura);
+    resultadoDiv.appendChild(minmax);
+    resultadoDiv.appendChild(presion);
+
+    cityInfo.appendChild(resultadoDiv);
+    if(tarjetaClima.classList.contains('hidden')){
+        tarjetaClima.classList.remove('hidden');
+    }
+}
+function limpiarHTML(){
+    tarjetaClima.classList.add('hidden');
+    while(cityInfo.firstChild){
+        cityInfo.removeChild(cityInfo.firstChild);
+    }
+}
+function kelvinToCelsius(kelvin) {
+    return Math.floor(kelvin - 273.15);
 }
 
 function imprimirAlerta(msg, tipo) {
